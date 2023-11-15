db712139482ab0ea046d4dce23840c0e9d3305b5
renzo01 <renzogomez577@gmail.com>
2023-11-15 00:54:35 -0500

    execute proyect

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ function App() {
     <h1>Computer Vision</h1>
     <p>Insert URL or Type prompt</p>
     <input placeholder='Enter URL to analyze or textual prompt to generate image'></input>
+    <br/>
     <button>Analyze</button>
     <button>Generate</button>
+    <hr/>
   </div>;
 }
 
