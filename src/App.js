032b5085957f3dfa3a94690d8d3ca6e55462475d
renzo01 <renzogomez577@gmail.com>
2023-11-15 00:20:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>
+    <h1>Computer Vision</h1>
+    <input placeholder='Enter URL to analyze or textual prompt to generate image'></input>
+  </div>;
 }
 
 export default App;
