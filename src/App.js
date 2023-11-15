9491c6231c62cd4d290b60af2f97b8d030f726f8
renzo01 <renzogomez577@gmail.com>
2023-11-15 00:27:11 -0500

    complete html struture

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 function App() {
   return <div>
     <h1>Computer Vision</h1>
+    <p>Insert URL or Type prompt</p>
     <input placeholder='Enter URL to analyze or textual prompt to generate image'></input>
+    <button>Analyze</button>
+    <button>Generate</button>
   </div>;
 }
 
